5726463a96b006af08ceece520daac35b16afc85
Mahadi Hassan <itsmahadi@gmail.com>
2021-05-17 13:29:29 +0600

    Updates Serializer

--- projectX/myapp/views.py
+++ projectX/myapp/views.py
@@ -7,6 +7,8 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
+# Importing EmployeeSerializer class from serializer
+from .serializer import EmployeeSerializer
 
 # Create your views here.
 # here below is functionn based view
@@ -39,13 +41,19 @@ class ListEmployee(APIView):
     def get(self, request):
         obj = Employee.objects.all()  # Query to get the values
         # getting the values in dictionary format below
-        data = {"responseValue": list(obj.values("id", "name"))}
-        return Response(data)
-        #return Response(data, status="", headers="", template_name="", content_type="")
+        #data = {"responseValue": list(obj.values("id", "name"))}
+        serializer_obj = EmployeeSerializer(obj, many=True)
+        return Response(serializer_obj.data)
+        # return Response(data, status="", headers="", template_name="", content_type="")
 
     def post(self, request):
-        name1 = request.data["name"]
-        obj = Employee(name=name1)
-        obj.save()
-        data = {"response": {"id": obj.id, "name": obj.name}}
-        return Response(data)
+        #name1 = request.data["name"]
+        #obj = Employee(name=name1)
+        # obj.save()
+        #data = {"response": {"id": obj.id, "name": obj.name}}
+        data = request.data
+        serializer_obj = EmployeeSerializer(data=data)
+        if serializer_obj.is_valid():
+            serializer_obj.save()
+            return Response(serializer_obj.data)
+        return Response(serializer_obj.errors)
